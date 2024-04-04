2a28f3853403df933c7cc7a317c2ab9c661f6cb3
Nuraasyikin <22FTT1504@student.pb.edu.bn>
2024-04-05 05:22:59 +0800

    add routers for login and sign up

--- src/app/start/start.page.ts
+++ src/app/start/start.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start',
@@ -9,18 +10,20 @@ import { Component, OnInit } from '@angular/core';
 
 export class StartPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   login() {
     // Implement login logic here (e.g., navigate to another page)
+    this.router.navigateByUrl('/login');
     console.log('Login button clicked!');
   }
 
   signup() {
     // Implement signup logic here (e.g., navigate to signup page)
+    this.router.navigateByUrl('/sign-up');
     console.log('Signup button clicked!');
   }
 
