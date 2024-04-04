7a1d9db6c2a519d67c128cc34a18c8d33983bf98
Nuraasyikin <22FTT1504@student.pb.edu.bn>
2024-04-05 07:37:28 +0800

    Add and modify sign-up page

---
+++ src/app/sign-up/sign-up.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.page.html',
+  styleUrls: ['./sign-up.page.scss'],
+})
+export class SignUpPage implements OnInit {
+  email: any;
+  password: any;
+  confirmPassword: any;
+
+  signup() {
+    // Implement signup logic here
+    console.log('Signup button clicked!');
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Confirm Password:', this.confirmPassword);
+
+    // You'd typically make an API call to a server to create a new user account here
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
