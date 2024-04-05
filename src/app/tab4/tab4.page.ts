e5b3ad2a3b28122c34af7e2ac42dbc98aa669758
Nuraasyikin <22FTT1504@student.pb.edu.bn>
2024-04-06 07:55:23 +0800

    Add settings in tab4

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab4',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  notificationsEnabled = true;
+
+  constructor(private router: Router) { }
+
+  goToTab1() {
+    this.router.navigateByUrl('/tabs/tab1');
+  }
 
   ngOnInit() {
   }
