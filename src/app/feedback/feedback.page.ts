6cd22dd7ef5320887712292b39ae5a4618a681e6
Nuraasyikin <22FTT1504@student.pb.edu.bn>
2024-04-07 07:54:49 +0800

    Finish modify the feedback

--- src/app/feedback/feedback.page.ts
+++ src/app/feedback/feedback.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-feedback',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.page.scss'],
 })
 export class FeedbackPage implements OnInit {
+  subject = '';
+  feedback = '';
+  name = '';
+  email = '';
 
-  constructor() { }
+  constructor(private toastController: ToastController) { }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your feedback has been submitted.',
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  submitFeedback() {
+    console.log('Name:', this.name);
+    console.log('Email:', this.email);
+    console.log('Subject:', this.subject);
+    console.log('Feedback:', this.feedback);
+
+    this.presentToast();
+  }
 
   ngOnInit() {
   }
-
 }
