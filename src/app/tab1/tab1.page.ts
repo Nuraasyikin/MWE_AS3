afe62dfa45173ef13e57942bf9ab97f2d28405b9
Nuraasyikin <22FTT1504@student.pb.edu.bn>
2024-04-07 23:20:30 +0800

    Modify tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+openFeedbackForm() {
+  this.router.navigate(['/feedback']);
+}
+openAnimalArchive() {
+  this.router.navigate(['/animals-archive']);
+}
 
-  constructor() {}
+openAnimals() {
+  this.router.navigate(['/animals-detect']);
+}
+
+openCalendar() {
+  this.router.navigate(['/animals-calendar']);
+}
+
+  openDiary() {
+    // Implement logic to open the Animals Diary page here
+    console.log('Animals Diary button clicked!');
+  }
+
+
+
+  constructor(private router: Router) {}
 
 }
